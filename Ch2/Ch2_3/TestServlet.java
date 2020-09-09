d01995ac1f4e906181fee4f77b5780aea3e75adf
xvpowerg <c911010083@gmail.com>
2020-09-09 21:22:22 +0800

    Update TestServlet.java

--- Ch2/Ch2_3/TestServlet.java
+++ Ch2/Ch2_3/TestServlet.java
@@ -2,9 +2,13 @@ package tw.com.web;
 
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 
 @WebServlet("/TestServlet")
 public class TestServlet extends HttpServlet{
-
+   public void doGet(HttpServletRequest req,HttpServletResponse resp) {
+	   
+   }
 }
