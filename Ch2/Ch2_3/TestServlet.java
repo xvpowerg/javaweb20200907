47d6bbc82a75cf17539b005e7fe2611f2303f3a0
xvpowerg <c911010083@gmail.com>
2020-09-09 22:03:04 +0800

    Update TestServlet.java

--- Ch2/Ch2_3/TestServlet.java
+++ Ch2/Ch2_3/TestServlet.java
@@ -1,14 +1,27 @@
 package tw.com.web;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
 @WebServlet("/TestServlet")
 public class TestServlet extends HttpServlet{
-   public void doGet(HttpServletRequest req,HttpServletResponse resp) {
+	//預設情況下都是doGet
+   public void doGet(HttpServletRequest req,HttpServletResponse resp) throws IOException {
+	   PrintWriter out = resp.getWriter();
+	   out.print("<HTML>");
+	   out.print("<BODY>");
+	   for (int i =1;i<=5;i++) {
+		   out.print("Howard");
+		   out.print("<br/>");
+	   }		
+		   out.print("Ken");
+	   out.print("</BODY>");
+	   out.print("</HTML>");
 	   
    }
 }
