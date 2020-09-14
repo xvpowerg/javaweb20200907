d2d87887a9b45796822650939659d24501b79357
xvpowerg <c911010083@gmail.com>
2020-09-14 20:31:51 +0800

    Update LoginServlet.java

--- Ch4/Ch4_1/LoginServlet.java
+++ Ch4/Ch4_1/LoginServlet.java
@@ -1,6 +1,7 @@
 package tw.com.web;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -12,10 +13,28 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/LoginServlet")
 public class LoginServlet extends HttpServlet {
-	@Override
-	protected void doGet(HttpServletRequest req, 
-			HttpServletResponse resp) throws ServletException, IOException {
-				
 	
-	}
+	private String account = "qwer";
+	private String password = "12345";
+//		@Override
+//		protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {				
+//			
+//			
+//		}
+		
+	
+	
+		@Override
+		protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+			 PrintWriter out =  resp.getWriter();
+			 out.println("doPost!!!");	
+			String acc =  req.getParameter("account");
+			String pass = req.getParameter("password");
+			
+			if (acc.equals(account) && pass.equals(password)) {
+				 out.println("登入成功");	
+			}else {
+				 out.println("登入失敗");
+			}
+		}
 }
