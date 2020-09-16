60d52ede0dea2f78bbc46a7be065503d443e9d08
xvpowerg <c911010083@gmail.com>
2020-09-16 20:41:16 +0800

    Update TestCheckBoxServlet.java

--- Ch5/Ch5_1_checkbox/TestCheckBoxServlet.java
+++ Ch5/Ch5_1_checkbox/TestCheckBoxServlet.java
@@ -2,6 +2,7 @@ package tw.com.web;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,12 +11,25 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 @WebServlet("/TestCheckBoxServlet")
 public class TestCheckBoxServlet extends HttpServlet{
+	
+
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		PrintWriter out =   resp.getWriter();
 		out.println("TestCheckBoxServlet!!");
+		
 		 String[] skills =  req.getParameterValues("skill");
 		//String skillType = req.getParameter("skill");
+		 
+		 Map<String,String[]> map = req.getParameterMap();		 
+		 map.forEach(
+				 (k,v)->{
+					 System.out.print(k+":");
+					 for (String value : v) {
+						 System.out.print(value+" ");
+					 }
+				 });
+		 
 		 for (String v : skills) {
 			 out.println("skillType:"+v);
 		 }
