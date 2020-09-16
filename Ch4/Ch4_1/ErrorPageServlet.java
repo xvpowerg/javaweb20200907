380508e163e209c4475d2f53ec934202f1492441
xvpowerg <c911010083@gmail.com>
2020-09-16 19:20:51 +0800

    Update ErrorPageServlet.java

--- Ch4/Ch4_1/ErrorPageServlet.java
+++ Ch4/Ch4_1/ErrorPageServlet.java
@@ -2,6 +2,8 @@ package tw.com.web;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -15,8 +17,15 @@ public class ErrorPageServlet extends HttpServlet {
 		@Override
 		protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 			resp.setContentType("text/html;charset=UTF-8");	//可顯示中文 要在PrintWriter 之前呼叫
-			PrintWriter out =  resp.getWriter();
+			PrintWriter  out =  resp.getWriter();
 			String msg = (String)req.getAttribute("msg");
+  			List<String>msgList = (ArrayList<String>)req.getAttribute("msgList");
+  			
 			out.println("ErrorPageServlet!!"+msg);
+			for (String msgTxt : msgList) {
+				//<p style='color:red;'> 文字變紅色
+				out.println("<p style='color:red;'>"+msgTxt+"</p>");
+			}
+			
 		}
 }
