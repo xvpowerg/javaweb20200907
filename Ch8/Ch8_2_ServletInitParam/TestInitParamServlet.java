850454bc8707f6c024195156033e4a7a6a8c5c3e
xvpowerg <c911010083@gmail.com>
2020-09-23 20:15:21 +0800

    Update TestInitParamServlet.java

--- Ch8/Ch8_2_ServletInitParam/TestInitParamServlet.java
+++ Ch8/Ch8_2_ServletInitParam/TestInitParamServlet.java
@@ -8,14 +8,18 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-@WebServlet("/TestInitParamServlet")
+//注意要設定name與xml一樣
+@WebServlet(name="TestInitParamServlet",urlPatterns = {"/TestInitParamServlet"})
 public class TestInitParamServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		PrintWriter out = resp.getWriter();
 		out.println("TestInitParamServlet!!");
-		
+		 String path = getInitParameter("path");
+		 System.out.println(path);
+		 String account = getInitParameter("account");
+		 System.out.println(account);
+		 
 	}
 }
