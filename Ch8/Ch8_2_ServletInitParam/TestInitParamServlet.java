3b891998c02467396700b5aac4833d532d89e79c
xvpowerg <c911010083@gmail.com>
2020-09-23 20:32:31 +0800

    Update TestInitParamServlet.java

--- Ch8/Ch8_2_ServletInitParam/TestInitParamServlet.java
+++ Ch8/Ch8_2_ServletInitParam/TestInitParamServlet.java
@@ -8,6 +8,10 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
 //注意要設定name與xml一樣
 @WebServlet(name="TestInitParamServlet",urlPatterns = {"/TestInitParamServlet"})
 public class TestInitParamServlet extends HttpServlet {
@@ -20,6 +24,11 @@ public class TestInitParamServlet extends HttpServlet {
 		 System.out.println(path);
 		 String account = getInitParameter("account");
 		 System.out.println(account);
+		 
+		 Path pathObj = Paths.get(path);		 
+		 Stream<Path> st =  Files.list(pathObj);
+		 st.forEach(p->{System.out.println(p);});
+		 
 		 
 	}
 }
