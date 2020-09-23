14e4e5cef06b4da2f2fd1a6d6b951b915bc2aafa
xvpowerg <c911010083@gmail.com>
2020-09-23 20:42:13 +0800

    Update TestInitParamServlet.java

--- Ch8/Ch8_2_ServletInitParam/TestInitParamServlet.java
+++ Ch8/Ch8_2_ServletInitParam/TestInitParamServlet.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -13,7 +14,12 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.stream.Stream;
 //注意要設定name與xml一樣
-@WebServlet(name="TestInitParamServlet",urlPatterns = {"/TestInitParamServlet"})
+@WebServlet(name="TestInitParamServlet",urlPatterns = {"/TestInitParamServlet"},
+	initParams= {		
+			@WebInitParam(name="color",value ="red"),
+			@WebInitParam(name="weight",value ="20"),
+			@WebInitParam(name="account",value ="zxcv")
+})
 public class TestInitParamServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -29,6 +35,10 @@ public class TestInitParamServlet extends HttpServlet {
 		 Stream<Path> st =  Files.list(pathObj);
 		 st.forEach(p->{System.out.println(p);});
 		 
+		 String color = getInitParameter("color");
+		 String weight = getInitParameter("weight");
+		 System.out.println(color);
+		 System.out.println(weight);
 		 
 	}
 }
