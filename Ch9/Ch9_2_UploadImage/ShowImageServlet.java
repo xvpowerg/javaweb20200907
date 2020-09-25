b7371837b7548fdf57f69ff09e98c62f01758130
xvpowerg <c911010083@gmail.com>
2020-09-25 21:10:42 +0800

    Add files via upload

--- Ch9/Ch9_2_UploadImage/ShowImageServlet.java
+++ Ch9/Ch9_2_UploadImage/ShowImageServlet.java
@@ -7,9 +7,13 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Stream;
 @WebServlet("/ShowImageServlet")
 public class ShowImageServlet extends HttpServlet{
@@ -21,9 +25,12 @@ public class ShowImageServlet extends HttpServlet{
 		System.out.println("imageFolder:"+imageFolder);
 		Path imageDir = Paths.get(imageFolder);
 		 Stream<Path> stream =  Files.list(imageDir);
-		 stream.forEach(p->{			 
-			 System.out.println(p);
+		 HttpSession session = req.getSession(); 
+		 List<String> list = new ArrayList<>();
+		 session.setAttribute("imageList", list);
+		 stream.forEach(p->{				
+			 list.add(p.getFileName().toString());
 		 });
-		
+		 resp.sendRedirect("showimage.jsp");
 	}
 }
---
+++ Ch9/Ch9_2_UploadImage/showimage.jsp
@@ -0,0 +1,13 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	<%= session.getAttribute("imageList") %>
+	<img src="images/image1.jpg"/>
+</body>
+</html>
