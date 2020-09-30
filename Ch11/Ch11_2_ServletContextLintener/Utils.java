baf9c0c8247d5c740011436b74181b390226aa64
xvpowerg <c911010083@gmail.com>
2020-09-30 20:22:18 +0800

    Add files via upload

---
+++ Ch11/Ch11_2_ServletContextLintener/Utils.java
@@ -0,0 +1,12 @@
+package tw.com.mod;
+
+public class Utils {
+    private static String imagePath;
+    
+    public static void setImagePath(String imagePath) {
+    	Utils.imagePath = imagePath;
+    }
+    public static String getImagePath() {
+    	return imagePath;
+    }
+}
