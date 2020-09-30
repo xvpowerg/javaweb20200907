5933eb15df646d4cda075578ec375539e15a8f1c
xvpowerg <c911010083@gmail.com>
2020-09-30 20:30:38 +0800

    Update TestContextLintener.java

--- Ch11/Ch11_2_ServletContextLintener/TestContextLintener.java
+++ Ch11/Ch11_2_ServletContextLintener/TestContextLintener.java
@@ -3,6 +3,8 @@ package tw.com.web;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
+import tw.com.mod.Utils;
+
 public class TestContextLintener implements ServletContextListener {
 
 	@Override
@@ -14,6 +16,9 @@ public class TestContextLintener implements ServletContextListener {
 	@Override
 	public void contextInitialized(ServletContextEvent sce) {
 		// TODO Auto-generated method stub
+		String imagepath = 
+				sce.getServletContext().getInitParameter("imagepath");
+		Utils.setImagePath(imagepath);
 		System.out.println("TestContextLintener contextInitialized ");
 	}
    
