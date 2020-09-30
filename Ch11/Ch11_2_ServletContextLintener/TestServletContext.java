c4869f08418abaae66c7724c5c3403827be4b408
xvpowerg <c911010083@gmail.com>
2020-09-30 20:44:56 +0800

    Update TestServletContext.java

--- Ch11/Ch11_2_ServletContextLintener/TestServletContext.java
+++ Ch11/Ch11_2_ServletContextLintener/TestServletContext.java
@@ -7,12 +7,15 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import tw.com.mod.Utils;
 @WebServlet("/TestServletContext")
 public class TestServletContext extends HttpServlet {
 	@Override
 	
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		resp.getWriter().println(Utils.getImagePath()); 
 	
 	}
 }
