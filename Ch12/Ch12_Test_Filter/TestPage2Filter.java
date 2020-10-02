8d3165c02af2dda419d7156bab9d675ce60edfce
xvpowerg <c911010083@gmail.com>
2020-10-02 20:05:06 +0800

    Add files via upload

---
+++ Ch12/Ch12_Test_Filter/TestPage2Filter.java
@@ -0,0 +1,24 @@
+package tw.com.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+@WebFilter("/Page2Servlet")
+public class TestPage2Filter implements Filter {
+
+	@Override
+	public void doFilter(ServletRequest request,
+			ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		System.out.println("TestPage2Filter in....");
+		chain.doFilter(request, response);
+		System.out.println("TestPage2Filter out....");
+	}
+
+}
