79916457cbde4e8da809d3580f1de504f5909d16
xvpowerg <c911010083@gmail.com>
2020-10-02 21:53:27 +0800

    Update Page1Servlet.java

--- Ch12/Ch12_TestCdi/Page1Servlet.java
+++ Ch12/Ch12_TestCdi/Page1Servlet.java
@@ -1,7 +1,10 @@
 package tw.com.web;
 
 import java.io.IOException;
+import java.util.List;
 
+import javax.inject.Inject;
+import javax.inject.Named;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -9,12 +12,19 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 @WebServlet("/Page1Servlet")
 public class Page1Servlet  extends HttpServlet{
-	
-	private Product p1 = new Book();
+	//注入
+	@Inject
+	private Product p1;
+	@Inject
+	//加入Qualifiers
+	//新增限定詞
+	@Named("animal")
+	private List<String> fruitList;
 			@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 				p1.setName("Android");
 				p1.setPrice(20);
-				System.out.println(p1);				
+				System.out.println(p1);	
+				System.out.println(fruitList);				
 	}
 }
