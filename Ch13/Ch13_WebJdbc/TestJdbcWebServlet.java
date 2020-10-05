00b21898b508f1769ac3a902b10210b63ceaad55
xvpowerg <c911010083@gmail.com>
2020-10-05 21:26:09 +0800

    add Ch13_WebJdbc

---
+++ Ch13/Ch13_WebJdbc/TestJdbcWebServlet.java
@@ -0,0 +1,31 @@
+package tw.com.web;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.Statement;
+import java.sql.SQLException;
+
+@WebServlet("/TestJdbcWebServlet")
+public class TestJdbcWebServlet extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String url ="jdbc:mysql://localhost:3306/db20201005?serverTimezone=CST&useSSL=false&allowPublicKeyRetrieval=true";
+		String user = "root";
+		String password = "123456";
+		try(Connection conn= DriverManager.
+				getConnection(url, user, password);){
+			
+		}catch(SQLException ex) {
+			System.out.println(ex);
+		}
+	}
+}
